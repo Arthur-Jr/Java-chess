312d22ec9eefc96588637525ac34c6c1bebc0a31
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-10 18:54:11 -0300

    Add board print on program

--- chess-system/src/app/Program.java
+++ chess-system/src/app/Program.java
@@ -1,10 +1,12 @@
 package app;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ChessMatch chess = new ChessMatch();
+		UI.printBoard(chess.getPieces());
 	}
 
 }
