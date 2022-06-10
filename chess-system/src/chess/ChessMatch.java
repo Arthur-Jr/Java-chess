f18528ada29404ee439e4c4d37dc8d18feaecda1
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-10 18:53:03 -0300

    feat: Initial implement of ChessMatch and ChessPiece

---
+++ chess-system/src/chess/ChessMatch.java
@@ -0,0 +1,22 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+	private Board board;
+
+	public ChessMatch() {
+		this.board = new Board(8, 8);
+	}
+	
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] pieces = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int row = 0; row < board.getRows(); row += 1) {
+			for (int col = 0; col < board.getColumns(); col += 1) {
+				pieces[row][col] = (ChessPiece) board.piece(row, col);
+			}
+		}
+		
+		return pieces;
+	}
+}
