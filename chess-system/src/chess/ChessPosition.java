7870380ffed083ec542c3ad48302cb918c33a960
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-13 18:47:12 -0300

    fix: Change argument position

--- chess-system/src/chess/ChessPosition.java
+++ chess-system/src/chess/ChessPosition.java
@@ -3,32 +3,32 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-	private int row;
 	private char column;
+	private int row;
 	
-	public ChessPosition(int row, char column) {
-		if (row < 'a' || row > 'h' || column < 1 || column > 8) {
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Invalid ChessPosition");
 		}
 		
-		this.row = row;
 		this.column = column;
-	}
-
-	public int getRow() {
-		return row;
+		this.row = row;
 	}
 
 	public char getColumn() {
 		return column;
 	}
 	
+	public int getRow() {
+		return row;
+	}
+
 	protected Position toPosition() {
 		return new Position(8 - this.row, this.column - 'a');
 	}
 	
 	protected static ChessPosition fromPostion(Position position) {
-		return new ChessPosition(8 - position.getRow(), (char)('a' + position.getColumn()));
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
