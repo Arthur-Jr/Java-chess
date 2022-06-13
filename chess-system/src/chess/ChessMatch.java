6b2497777fa319f96d0ee51bbe9e7900872e7f74
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-13 18:48:36 -0300

    feat: Initial setup implement

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -1,12 +1,19 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Knight;
+import chess.pieces.Pawn;
+import chess.pieces.Queen;
+import chess.pieces.Rock;
 
 public class ChessMatch {
 	private Board board;
 
 	public ChessMatch() {
 		this.board = new Board(8, 8);
+		this.initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -18,5 +25,35 @@ public class ChessMatch {
 		}
 		
 		return pieces;
+	}
+	
+	private void placePiece(char col, int row, ChessPiece piece) {
+		this.board.placePiece(piece, new ChessPosition(col, row).toPosition());
+	}
+	
+	private void initialSetup() {
+		placePiece('a', 1, new Rock(board, Color.WHITE));
+		placePiece('b', 1, new Knight(board, Color.WHITE));
+		placePiece('c', 1, new Bishop(board, Color.WHITE));
+		placePiece('d', 1, new Queen(board, Color.WHITE));
+		placePiece('e', 1, new King(board, Color.WHITE));
+		placePiece('f', 1, new Bishop(board, Color.WHITE));
+		placePiece('g', 1, new Knight(board, Color.WHITE));
+		placePiece('h', 1, new Rock(board, Color.WHITE));
+		for (int i = 0; i < this.getPieces().length; i += 1) {
+			placePiece((char)('a' + i), 2, new Pawn(board, Color.WHITE));
+		}
+		
+		placePiece('a', 8, new Rock(board, Color.BLACK));
+		placePiece('b', 8, new Knight(board, Color.BLACK));
+		placePiece('c', 8, new Bishop(board, Color.BLACK));
+		placePiece('d', 8, new Queen(board, Color.BLACK));
+		placePiece('e', 8, new King(board, Color.BLACK));
+		placePiece('f', 8, new Bishop(board, Color.BLACK));
+		placePiece('g', 8, new Knight(board, Color.BLACK));
+		placePiece('h', 8, new Rock(board, Color.BLACK));
+		for (int i = 0; i < this.getPieces().length; i += 1) {
+			placePiece((char)('a' + i), 7, new Pawn(board, Color.BLACK));
+		}
 	}
 }
